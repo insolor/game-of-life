482dd5870cbd8c6aa47dd03ee5606c3e811d2b62
insolor <2442833+insolor@users.noreply.github.com>
2024-04-26 21:28:04 +0300

    Basic Field implementation

--- game_of_life/models.py
+++ game_of_life/models.py
@@ -1,6 +1,9 @@
 from bitarray import bitarray
 
 
+SIZE = 64
+
+
 class Block:
     __slots__ = ("x", "y", "rows")
 
@@ -12,13 +15,47 @@ class Block:
     def __init__(self, x, y) -> None:
         self.x = x
         self.y = y
-        self.rows = tuple(bitarray(32) for _ in range(32))
+        self.rows = tuple(bitarray(SIZE) for _ in range(SIZE))
 
     @property
-    def coords(self):
+    def coords(self) -> tuple[int, int]:
         return (self.x, self.y)
+    
+    def __setitem__(self, local_coords: tuple[int, int], value: int) -> None:
+        ...
+    
+    def __getitem__(self, local_coords: tuple[int, int]) -> int:
+        ...
 
 
 class Field:
     __slots__ = ("blocks",)
     blocks: dict[tuple[int, int], Block]
+
+    def __init__(self) -> None:
+        self.blocks = dict()
+
+    @staticmethod
+    def convert_coords(x: int, y: int) -> tuple[tuple[int, int], tuple[int, int]]:
+        return tuple(zip(divmod(x, SIZE), divmod(y, SIZE)))
+    
+    def __setitem__(self, coords: tuple[int, int], value: int) -> None:
+        x, y = coords
+        block_coords, local_coords = self.convert_coords(x, y)
+        
+        block = self.blocks.get(block_coords)
+        if not block:
+            block = Block(*block_coords)
+            self.blocks[block_coords] = block
+        
+        block[local_coords] = value
+    
+    def __getitem__(self, coords: tuple[int, int]) -> int:
+        x, y = coords
+        block_coords, local_coords = self.convert_coords(x, y)
+        
+        block = self.blocks.get(block_coords)
+        if not block:
+            return 0
+        
+        return block[local_coords]
