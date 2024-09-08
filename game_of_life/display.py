85f2c40ab5485db3e8952d743ffc0c0122d33d8d
insolor <2442833+insolor@users.noreply.github.com>
2024-09-08 19:09:29 +0300

    Display block by block instead of cells

--- game_of_life/display.py
+++ game_of_life/display.py
@@ -1,8 +1,12 @@
 from dataclasses import dataclass
+from typing import TYPE_CHECKING
 
 import pyxel
 
-from game_of_life.models import Field
+from game_of_life.models import BLOCK_SIZE
+
+if TYPE_CHECKING:
+    from game_of_life.models import Block, Field
 
 DRAW_COLOR = pyxel.COLOR_WHITE
 
@@ -22,18 +26,40 @@ class DisplayParams:
         self.scale = new_scale
 
 
-def display_field(field: Field, params: DisplayParams) -> None:
-    x_start, x_offset = divmod(-params.pixel_offset_x, params.scale)
-    y_start, y_offset = divmod(-params.pixel_offset_y, params.scale)
-    x_end = x_start + params.width / params.scale
-    y_end = y_start + params.height / params.scale
+def display_field(field: "Field", params: DisplayParams) -> None:
+    block_pixel_size = BLOCK_SIZE * params.scale
 
-    for x in range(int(x_start), int(x_end) + 1):
-        screen_x = (x - x_start) * params.scale - x_offset
-        for y in range(int(y_start), int(y_end) + 1):
-            if field[x, y]:
-                screen_y = (y - y_start) * params.scale - y_offset
-                if params.scale <= 1:
-                    pyxel.pset(screen_x, screen_y, DRAW_COLOR)
-                else:
-                    pyxel.rectb(screen_x, screen_y, params.scale, params.scale, DRAW_COLOR)
+    for (block_x, block_y), block in field.blocks.items():
+        block_screen_x = block_x * block_pixel_size + params.pixel_offset_x
+        if (block_screen_x + block_pixel_size) < 0 or block_screen_x > params.width:
+            continue
+
+        block_screen_y = block_y * block_pixel_size + params.pixel_offset_y
+        if (block_screen_y + block_pixel_size) < 0 or block_screen_y > params.height:
+            continue
+
+        display_block(block, params, block_screen_x, block_screen_y)
+
+
+def display_block(block: "Block", params: DisplayParams, screen_x: int, screen_y: int) -> None:
+    for y, row in enumerate(block.rows):
+        if not row.value:
+            continue
+
+        cell_screen_y = screen_y + y * params.scale
+        if not (0 < cell_screen_y < params.height):
+            continue
+
+        for x, cell in enumerate(row):
+            if not cell:
+                continue
+
+            cell_screen_x = screen_x + x * params.scale
+
+            if not (0 < cell_screen_x < params.width):
+                continue
+
+            if params.scale <= 1:
+                pyxel.pset(cell_screen_x, cell_screen_y, DRAW_COLOR)
+            else:
+                pyxel.rectb(cell_screen_x, cell_screen_y, params.scale, params.scale, DRAW_COLOR)
